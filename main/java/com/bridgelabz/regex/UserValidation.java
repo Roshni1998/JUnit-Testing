9e1e16f7555084152dc183c8f667418575fc2e0e
Roshni1998 <roshnimali847@gmail.com>
2021-06-18 02:51:36 +0530

    Added: User Registration UC-1

--- main/java/com/bridgelabz/regex/UserValidation.java
+++ main/java/com/bridgelabz/regex/UserValidation.java
@@ -1,9 +1,24 @@
 package com.bridgelabz.regex;
 
-    public class UserValidation {
+import java.util.regex.Pattern;
+/**
+ * @author ROSHNI
+ * User Registration Problem
+ */
 
-        public static void main (String args[]){
-            System.out.println("Welcome to User Registration Problem");
-        }
+public class UserValidation {
+
+    private static final String FIRST_NAME_PATTERN = "^[A-Z][a-z]{3,15}$";
+
+    /*UC-1*/
+    //First name starts with Cap and has minimum 3 characters
+    public boolean isValidFirstName(String fname) {
+        /**
+         * @param firstName
+         * @return boolean
+         */
+        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+        return pattern.matcher(fname). matches();
     }
+}
 
