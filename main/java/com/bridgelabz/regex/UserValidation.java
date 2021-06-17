3f07d9c5ffad9bb2b5b4bb753a4d5a797d710e24
Roshni1998 <roshnimali847@gmail.com>
2021-06-18 02:42:55 +0530

    Added: Welcome Message

---
+++ main/java/com/bridgelabz/regex/UserValidation.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.regex;
+
+    public class UserValidation {
+
+        public static void main (String args[]){
+            System.out.println("Welcome to User Registration Problem");
+        }
+    }
+
