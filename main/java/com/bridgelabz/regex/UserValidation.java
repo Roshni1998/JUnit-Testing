66698dd342259268229d8a65e09d8392ff4b4564
Roshni1998 <roshnimali847@gmail.com>
2021-06-18 10:43:44 +0530

    Added: User Registration UC-3

--- main/java/com/bridgelabz/regex/UserValidation.java
+++ main/java/com/bridgelabz/regex/UserValidation.java
@@ -9,6 +9,7 @@ import java.util.regex.Pattern;
 public class UserValidation {
 
     private static final String NAME_PATTERN = "^[A-Z][a-z]{3,15}$";
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([.+-_][0-9a-zA-Z]+)*"+"@([a-zA-Z0-9][-]?)+[.][a-zA-Z]{2,4}+([.][a-zA-Z]{0,2})?$";
 
     /*UC-1*/
     //First name starts with Cap and has minimum 3 characters
@@ -30,6 +31,17 @@ public class UserValidation {
          */
         Pattern pattern = Pattern.compile(NAME_PATTERN);
         return pattern.matcher(lname). matches();
+    }
+    /*UC-3*/
+    //Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
+    public boolean isValidEmailId(String emailId) {
+        /**
+         *
+         * @param emailId
+         * @return boolean
+         */
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matcher(emailId). matches();
     }
 
 }
