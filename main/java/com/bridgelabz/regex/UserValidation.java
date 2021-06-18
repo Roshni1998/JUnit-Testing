d2f762c85ca88139b35fb22be02a1a470e856823
Roshni1998 <roshnimali847@gmail.com>
2021-06-18 11:29:04 +0530

    Added: User Registration UC-5

--- main/java/com/bridgelabz/regex/UserValidation.java
+++ main/java/com/bridgelabz/regex/UserValidation.java
@@ -11,6 +11,7 @@ public class UserValidation {
     private static final String NAME_PATTERN = "^[A-Z][a-z]{3,15}$";
     private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([.+-_][0-9a-zA-Z]+)*"+"@([a-zA-Z0-9][-]?)+[.][a-zA-Z]{2,4}+([.][a-zA-Z]{0,2})?$";
     private static final String MOBILE_NUMBER_PATTERN = "(0|91)?\\s([7-9][0-9]{9})";
+    private static final String PASSWORD_PATTERN = "^.{8,12}$";
 
     /*UC-1*/
     //First name starts with Cap and has minimum 3 characters
@@ -54,6 +55,17 @@ public class UserValidation {
     public boolean isValidMobileNo(String mobileNo) {
         Pattern pattern = Pattern.compile(MOBILE_NUMBER_PATTERN);
         return pattern.matcher(mobileNo). matches();
+    }
+    /*UC-5*/
+    //Password should contain minimum 8 Characters
+    public static boolean isValidPassword(String password) {
+        /**
+         *
+         * @param password
+         * @return boolean
+         */
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        return pattern.matcher(password). matches();
     }
 
 }
