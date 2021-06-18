ab5fc0d1761e1e8a1b1d5661bf939dff16de2cf7
Roshni1998 <roshnimali847@gmail.com>
2021-06-18 12:19:07 +0530

    Added: User Registration UC-8

--- main/java/com/bridgelabz/regex/UserValidation.java
+++ main/java/com/bridgelabz/regex/UserValidation.java
@@ -11,7 +11,7 @@ public class UserValidation {
     private static final String NAME_PATTERN = "^[A-Z][a-z]{3,15}$";
     private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([.+-_][0-9a-zA-Z]+)*"+"@([a-zA-Z0-9][-]?)+[.][a-zA-Z]{2,4}+([.][a-zA-Z]{0,2})?$";
     private static final String MOBILE_NUMBER_PATTERN = "(0|91)?\\s([7-9][0-9]{9})";
-    private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z]).{8,12}$";
+    private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&-+=()]).{8,12}$";
 
     /*UC-1*/
     //First name starts with Cap and has minimum 3 characters
@@ -56,10 +56,11 @@ public class UserValidation {
         Pattern pattern = Pattern.compile(MOBILE_NUMBER_PATTERN);
         return pattern.matcher(mobileNo). matches();
     }
-    /*UC-5, UC-6, UC-7*/
+    /*UC-5, UC-6, UC-7, UC-8*/
     /* Password should contain minimum 8 Characters
      * Password should have at least 1 Upper Case
      * Password should have at least 1 numeric number
+     * Should have exactly 1 Special Character
      */
     /**
      * @param password
@@ -69,6 +70,8 @@ public class UserValidation {
         Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
         return pattern.matcher(password). matches();
     }
+
+
 
 }
 
